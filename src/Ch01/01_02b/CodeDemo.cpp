0a86687d444baa1141d7d38c038648ac08910318
mike-balajeiwcz <mbalajew@uwaterloo.ca>
2025-06-06 20:23:01 +0000

    my first ever c++ program

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    //first c++ program
+    std::cout << "Hello World!";
+    std::cout << std::endl;
+    std::cout << std::endl;
+    return 0;
+}
