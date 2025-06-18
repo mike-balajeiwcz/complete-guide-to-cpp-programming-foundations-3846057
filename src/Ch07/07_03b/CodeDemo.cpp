097462404d21a0355e47c4c6b063ae98a718c756
mike-balajeiwcz <mbalajew@uwaterloo.ca>
2025-06-18 16:27:21 +0000

    changed older code

--- src/Ch07/07_03b/CodeDemo.cpp
+++ src/Ch07/07_03b/CodeDemo.cpp
@@ -7,11 +7,61 @@
 #include <string>
 #include <algorithm>
 
-using namespace std;
-
 class Inventory{
-private:
-    std::vector<std::string> *items; // Pointer to a vector of items
+public:
+    // Add an item to the inventory
+    void addItem(const std::string &item){
+        if(items->size() == capacity) {
+            std::cout << "Cannot add " << item << " since invectory is full!" << std::endl;
+        } else {
+            items->push_back(item);
+        }
+    }
+
+    // Remove item from inventory
+    void removeItem(const std::string &item) {
+        std::vector<std::string>::iterator it = std::find(items->begin(), items->end(), item);
+        //or use auto!
+        // auto it = std::find(items->begin(), items->end(), item);
+        if(it != items->end()) {
+            items->erase(it);
+        } else {
+            std::cout << "Cannot find " << item << "." << std::endl;
+        }
+    }
+
+    // getItem() takes a int, depending: returns std::string or produces output.
+    // const qualifier prevents objects inside this function from modification.
+    std::string getItem(const int &index) const {
+        if (index < 0 || index > items->size() - 1) {
+            std::cout << "Index is not in range of inventory!" << std::endl;
+        } else {
+            return (*items)[index];
+        }
+    }
+
+    // getItemCount() returns number of elements in items vector as int.
+    int getItemCount(void) const {
+        return items->size();
+    }
+
+    // displayInventory() returns void, depending: produced output.
+    void displayInventory() const {
+        std::cout << "Inventory: [ ";
+        int size = items->size();
+        for (int i = 0; i < size; ++i) {
+            std::cout << (*items)[i];
+            if(i != size - 1) {
+               std::cout << ", "; 
+            }
+        }
+        std::cout << " ]" << std::endl;
+    }
+
+private: 
+    // Pointer to a vector of items (array of items)
+    //without using namespace std;
+    std::vector<std::string> *items; 
     int capacity; // Maximum number of items allowed
 };
 
