fd3bcce931856c4fa66ff6b67504eaaa7d01f135
mike-balajeiwcz <mbalajew@uwaterloo.ca>
2025-06-18 19:59:45 +0000

    Changed functions to use operator overloading

--- src/Ch07/07_07b/inventory.cpp
+++ src/Ch07/07_07b/inventory.cpp
@@ -23,25 +23,27 @@ Inventory::~Inventory(){
     delete items; // Prevent memory leak by deallocating the dynamic vector
 }
 
-// Add item to inventory
-void Inventory::addItem(const std::string& item){
+// Overloaded += operator
+Inventory &Inventory::operator+=(const std::string& item){
     if (items->size() < capacity)
         items->push_back(item);
     else
         std::cout << "Inventory is full, cannot add " << item << std::endl;
+    return *this;
 }
 
-// Remove item from inventory
-void Inventory::removeItem(const std::string& item){
+// overloaded -= operator to remove item 
+Inventory& Inventory::operator-=(const std::string& item){
     auto it = std::find(items->begin(), items->end(), item);
     if (it != items->end())
         items->erase(it);
     else
         std::cout << "Item " << item << " not found in inventory" << std::endl;
+    return *this;
 }
 
-// Access item by index
-std::string Inventory::getItem(int index) const{
+// Overloaded [] operator to access item by index
+std::string Inventory::operator[](int index) const{
     if (index >= 0 && index < items->size())
         return (*items)[index];
     else
