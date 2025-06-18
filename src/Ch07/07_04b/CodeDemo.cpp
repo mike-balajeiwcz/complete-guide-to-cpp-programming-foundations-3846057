1b388cbb5307fb11c5f869380991a6db3ebc7b2b
mike-balajeiwcz <mbalajew@uwaterloo.ca>
2025-06-18 16:51:20 +0000

    Finished implementing constructors and destrcutors.

--- src/Ch07/07_04b/CodeDemo.cpp
+++ src/Ch07/07_04b/CodeDemo.cpp
@@ -10,20 +10,48 @@
 class Inventory{
 public:
     // Default Constructor
-    Inventory(){
-        capacity = 10;
+    Inventory(): capacity(10){
+        // the ": capacity(10" is equivalient to "capacity = 10;" written in body of code.
+
+        // another way to initalize capacity without using the intalizer list thing
+        // capacity = 10;
+
+        // since we have no parameters we can also say:
+        // this->capacity = 10;
+        // since we are refering to "this" particular instance/object of this class.
+        // but we dont need to since data members will always be able to be used 
+        // within functions within its class. (even if private or protected)
+
         items = new std::vector<std::string>();
     }
 
+    // a constructor has no return type and must be the same name as the class (Inventory in this case)
+    // the new keyword is similar to malloc() in C. It returns a pointer to allocated memory 
+    // in the heap based on the size of memory needed. In this case we are allocating memory for 
+    // a vector of strings. (malloc requires you to specify the exact size in bytes)
+    // the new keyword must somehow dynamicaly re-size.
+    // when the new keyword is used, the memory must be freed with the delete keyword.
+
+    // this keyword is a pointer to "this" particular instance of the class.
+    // so you can access the data or function members of "this" instance of the class (this object)
+
     // Overloaded Constructor
-    Inventory(int capacity){
-        this->capacity = capacity;
+    Inventory(int capacity_i): capacity(capacity_i){
+        // we add "_i" for redability as the parameter we are using to intialize hence the "i".
+
+        // again we can use this instead of the intialize list in the constructor declaration line
+        // this->capacity = capacity_i;
         items = new std::vector<std::string>();
     }
 
     // Destructor
     ~Inventory(){
-        delete items; // Prevent memory leak by deallocating the dynamic vector
+        // tilde charachter "~" indicates a destructor (same syntax as constructor other than that) 
+        delete items;
+
+        // as I mentioned before, whenever the "new" keyword is used, "delete" needs to be used
+        // its the same as using free() for whatever you malloc()
+        // if not familiar with free, esential the "delete" keyword will de-allocate the memoray in the heap that was allocated using "new"
     }
 
     // Add item to inventory
