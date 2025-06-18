55209bc8193be5212b2fca33fdc0c9345e35aac2
mike-balajeiwcz <mbalajew@uwaterloo.ca>
2025-06-18 19:49:59 +0000

    modulrization

---
+++ src/Ch07/07_07b/inventory.h
@@ -0,0 +1,44 @@
+#pragma once
+// not all compilers support pragma (but vscode does)
+// include guard works with all compilers
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class Inventory{
+public:
+    // Default Constructor
+    Inventory();
+
+    // Overloaded Constructor
+    Inventory(int capacity_i);
+
+    // Destructor
+    ~Inventory();
+
+    // Add item to inventory
+    void addItem(const std::string& item);
+
+    // Remove item from inventory
+    void removeItem(const std::string& item);
+
+    // Access item by index
+    std::string getItem(int index) const;
+
+    // Get number of items in the inventory
+    int getItemCount() const;
+
+    // Display inventory contents
+    void displayInventory() const;
+
+private:
+    std::vector<std::string> *items; // Pointer to a vector of items
+    int capacity; // Maximum number of items allowed
+};
+
+
+#endif // INVENTORY_H
